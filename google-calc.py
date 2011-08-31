32ef8a5fa63ebd1c7cab3deaa6b6b58183d614c7
BirdAPI <birdapi@gmail.com>
2011-08-30 21:13:25 -0700

    performs a loop to evaluate expressions

--- google-calc.py
+++ google-calc.py
@@ -4,8 +4,18 @@ from Google import google
 from Google.google import Google
 
 def main():
-    print "__main__"
-    print Google.calculate("1 + 2")
+    prev = None
+    print "Enter an expression, or 'quit' to exit.\n"
+    while True:
+        string = raw_input()
+        if string == "quit":
+            break
+        result = Google.calculate(string)
+        if result:
+            print "=", result.value, result.unit if result.unit else ""
+        else:
+            print "No Result!"
+        prev = result
 
 if __name__ == "__main__":
     main()
