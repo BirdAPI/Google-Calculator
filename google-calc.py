9d12f9298f42dc79110161e0a58cec7ca41eb09e
BirdAPI <birdapi@gmail.com>
2011-08-30 20:55:23 -0700

    added google search api submodule, and the proper imports in google-calc.py

--- google-calc.py
+++ google-calc.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python
 
+from Google import google
+from Google.google import Google
+
+def main():
+    print "__main__"
+    print Google.calculate("1 + 2")
+
+if __name__ == "__main__":
+    main()
